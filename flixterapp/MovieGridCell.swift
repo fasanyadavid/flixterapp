f20b00be9e2430039c7d91a858e4cd30a1c48aa9
David Fasanya <david.fasanya@ttu.edu>
2020-10-02 18:48:10 -0500

    Details Screen and Collection View Added

---
+++ flixterapp/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixterapp
+//
+//  Created by David Fasanya on 10/2/20.
+//  Copyright © 2020 fasanyadavid. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
